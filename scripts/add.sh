ef965b67653d09807aa3e4485b0b16b958e34442
Oscar Renalias <oscar@renalias.net>
2013-12-21 01:23:14 +0200

    Added the correct content type.

--- scripts/add.sh
+++ scripts/add.sh
@@ -1,1 +1,2 @@
-curl -X PUT http://localhost:8053/lookup -d '{ "ip": "1.2.3.4", "host": "www.test.com", "TTL": "10", "expires": "3600", "type": "A" }'
+curl -X PUT -H "Content-Type: application/json" \
+http://localhost:8053/lookup -d '{ "ip": "1.2.3.4", "host": "www.test.com", "ttl": "10", "expires": "3600", "type": "A" }'
