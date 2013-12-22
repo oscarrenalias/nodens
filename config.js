f97f20103786e07c4c60090b8767aa371fa9bd63
Oscar Renalias <oscar@renalias.net>
2013-12-22 14:18:56 +0200

    Added comments as well as configuration options for the DNS client settings (currently timeout is the only attribute exposed).

--- config.js
+++ config.js
@@ -1,18 +1,28 @@
 module.exports = {
+    // configuration for storage, should not need to be modified
     storage: {
         dbPath: './db',
         collName: 'lookups'
     },
 
+    // DNS and REST API ports for the server
     ports: {
         api: 8053,
         dns: 15353
     },
 
     dns: {
+        // configuration for the DNS upstream
         upstream: {
+            // enabled or disabled
             enabled: true,
-            ip: '8.8.8.8'
+            // IP address, default one of Google's
+            ip: '8.8.8.8',
+            // options for the DNS client
+            client: {
+                // timeout in milliseconds for each request
+                timeout: 3000
+            }
         }
     }
 };
