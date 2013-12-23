4e7579b4bd38a01e65d2c98f9e2267f1eb07ee9c
Oscar Renalias <oscar@renalias.net>
2013-12-24 01:47:00 +0200

    Add more test records

--- scripts/add.sh
+++ scripts/add.sh
@@ -1,2 +1,8 @@
 curl -X PUT -H "Content-Type: application/json" \
 http://localhost:8053/lookup -d '{ "address": "1.2.3.4", "name": "www.test.com", "ttl": "10", "expires": "3600", "type": 1 }'
+
+curl -X PUT -H "Content-Type: application/json" \
+http://localhost:8053/lookup -d '{ "address": "1.2.3.5", "name": "www.test1.com", "ttl": "10", "expires": "3600", "type": 1 }'
+
+curl -X PUT -H "Content-Type: application/json" \
+http://localhost:8053/lookup -d '{ "address": "1.2.3.6", "name": "www.test1.com", "ttl": "10", "expires": "3600", "type": 1 }'
