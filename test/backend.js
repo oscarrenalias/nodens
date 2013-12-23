b06360969a0fa5af0c262976e77f5e80e856273a
Oscar Renalias <oscar@renalias.net>
2013-12-23 23:55:11 +0200

    Added support for multiple addresses per host

--- test/backend.js
+++ test/backend.js
@@ -11,7 +11,15 @@ var testLookup = {	// test lookup data
 	name: "www.test.com",
 	TTL: "10",
     expires: "3600",
-	type: "A"
+	type: 1
+} 	
+
+var testLookup2 = {
+	address: "1.2.3.5",
+	name: "www.test.com",
+	TTL: "10",
+    expires: "3600",
+	type: 1
 }
 
 // Helper function to force an error
@@ -25,8 +33,8 @@ module.exports = {
 
 		store.updateLookup(testLookup).then(function(item) {
 			return(store.doLookup('www.test.com'));
-		}).then(function(result) {
-			test.equal(result[0].ip, testLookup.ip);
+		}).then(function(result) {;
+			test.equal(result[0].address, testLookup.address);
 		}).fail(failed).finally(test.done);
 	},
 
@@ -46,6 +54,15 @@ module.exports = {
 		}).then(function(result) {
             // null indicates that it wasn't found
 			test.equal(result[0], null);
+		}).fail(failed).finally(test.done);
+	},
+
+	testMultipleAddressesPerName: function(test) {
+		test.expect(1);
+		store.updateLookup(testLookup2).then(function(item) {
+			return(store.doLookup('www.test.com'));
+		}).then(function(result) {
+			test.equal(result.length, 2);
 		}).fail(failed).finally(test.done);
 	}
 }
