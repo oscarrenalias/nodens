bd9b711dc44d4a84af3ec1b562552f1f7c212dff
Oscar Renalias <oscar@renalias.net>
2013-12-30 22:56:39 +0200

    Fixes recommended by JsHint

--- test/backend.js
+++ test/backend.js
@@ -10,17 +10,17 @@ var assert = require('assert'),
 
 var testLookups = [
 	{	// test lookup data
-		address: "1.2.3.4",
-		name: "www.test.com",
-		TTL: "10",
-	    expires: "3600",
+		address: '1.2.3.4',
+		name: 'www.test.com',
+		TTL: '10',
+	    expires: '3600',
 		type: 1
 	},
 	{
-		address: "1.2.3.5",
-		name: "www.test.com",
-		TTL: "10",
-	    expires: "3600",
+		address: '1.2.3.5',
+		name: 'www.test.com',
+		TTL: '10',
+	    expires: '3600',
 		type: 1
 	},
 	{
@@ -30,15 +30,15 @@ var testLookups = [
 		expires: '3600',
 		type: 1
 	}
-]
+];
 		
 // Helper function to force an error
 function failed(err) {
-	console.log("Promise failed with error: " + err);
+	console.log('Promise failed with error: ' + err);
 }
 
 module.exports = {
-	"Test update and lookup": function(test) {
+	'Test update and lookup': function(test) {
 		test.expect(1);
 		store.updateLookup(testLookups[0]).then(function(item) {
 			return(store.doLookup('www.test.com'));
@@ -47,14 +47,14 @@ module.exports = {
 		}).fail(failed).finally(test.done);
 	},
 
-	"Test a reverse lookup": function(test) {
+	'Test a reverse lookup': function(test) {
 		test.expect(1);
 		store.doReverseLookup('1.2.3.4').then(function(result) {
 			test.equal(result.host, testLookups[0].host)
 		}).fail(failed).finally(test.done);
 	},
 
-	"Test a host with multiple addresses for a single name record": function(test) {
+	'Test a host with multiple addresses for a single name record': function(test) {
 		test.expect(1);
 		store.updateLookup(testLookups[1]).then(function(item) {
 			return(store.doLookup('www.test.com'));
@@ -63,7 +63,7 @@ module.exports = {
 		}).fail(failed).finally(test.done);
 	},
 	
-	"Test deletion based on address": function(test) {
+	'Test deletion based on address': function(test) {
 		test.expect(1);
 		store.deleteAddress(testLookups[1].address).then(function() {
 			return(store.doLookup('www.test.com'))
@@ -72,7 +72,7 @@ module.exports = {
 		}).fail(failed).finally(test.done)
 	},
 	
-	"Test deletion based on name": function(test) {
+	'Test deletion based on name': function(test) {
 		test.expect(1);
 
 		store.deleteName(testLookups[0].name).then(function(item) {
