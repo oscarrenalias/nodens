c06a8d7a62b7d3f3e0d2824ef1be5fc672a56043
Oscar Renalias <oscar@renalias.net>
2014-01-06 23:27:43 +0200

    Ensure that a default value for expiration time is provided, and that value can now be provided from the configuration file via api.defaultExpires.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var DNSServer = require("./lib/dns"),
     config = require("./config"),
     backend = new BackendStorage(config.storage),
     dnsServer = new DNSServer(backend, config.dns),
-    apiServer = new APIServer(backend),
+    apiServer = new APIServer(backend, config.api),
     dnsPort = process.env.DNSPORT || config.ports.dns,
     apiPort = process.env.APIPORT || config.ports.api;
 
